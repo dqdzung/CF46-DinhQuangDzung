f7e6746c8c713994c259d7e756fa1c9eb841fb6a
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-03-07 13:53:56 +0700

    practice

---
+++ Practice/jQuery practice/index.js
@@ -0,0 +1,15 @@
+// $("h1").hide();
+// $("#heading1").hide();
+// $(".heading2").hide();
+// $("span").css("color", "red");
+
+$("ul#list li:first").css("color", "red");
+$("ul#list li:last").css("color", "green");
+$("ul#list li:even").css("background-color", "yellow");
+$("ul#list li:odd").css("background-color", "#cccccc");
+
+// $(":button").hide();
+// $(":text").hide();
+
+$("[href]").css("color", "red");
+$("a[href='http://youtube.com']").css("color", "green");

804c324b1f020334b5e7eba2ff48aff29bc2a77b
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-03-22 13:58:53 +0700

    css

--- Sess11 (19th March) - React/gif-search/src/App.js
+++ Sess11 (19th March) - React/gif-search/src/App.js
@@ -1,27 +1,53 @@
-import logo from "./images/GIPHY Logo 60px.png";
+import logo from "./images/giphy-logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-        <img src={logo} alt="" />
-        <h1>Search a Gif</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button id="search-btn">Search</button>
-      </div>
-      <div className="content">
-        <div className="image-container">
-          <div className="image-content">
-            <img
-              src="https://media.giphy.com/media/LmYoDjbWL0RkFcRoeb/giphy.gif"
-              alt=""
-            />
+    <div className="App d-flex justify-content-center">
+      <div id="main-container" className="d-flex flex-column align-items-center">
+        <div className="header">
+          <img src={logo} alt="" />
+          <h1>Search a Gif</h1>
+        </div>
+        <div className="form">
+          <input type="text" className="form-control" />
+          <button id="search-btn" className="btn btn-primary d-none">
+            Search
+          </button>
+        </div>
+        <div className="content">
+          <div className="image-container d-flex">
+            <div className="image-inner">
+              <img
+                src="https://media.giphy.com/media/LmYoDjbWL0RkFcRoeb/giphy.gif"
+                alt=""
+              />
+            </div>
+            <div className="image-caption">
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            </div>
           </div>
-          <div className="image-caption">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <div className="image-container d-flex">
+            <div className="image-inner">
+              <img
+                src="https://media.giphy.com/media/H0EBDM4Vk6880/giphy.gif"
+                alt=""
+              />
+            </div>
+            <div className="image-caption">
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            </div>
+          </div>
+          <div className="image-container d-flex">
+            <div className="image-inner">
+              <img
+                src="https://media.giphy.com/media/Rxt5Nxbl3KM3DhyxZm/giphy.gif"
+                alt=""
+              />
+            </div>
+            <div className="image-caption">
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+            </div>
           </div>
         </div>
       </div>

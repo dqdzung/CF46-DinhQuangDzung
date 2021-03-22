0529d9d896cd2a529cce9e2ff8b033bbc41b0616
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2021-03-22 22:18:50 +0700

    class

--- Sess11 (19th March) - React/gif-search/src/App.js
+++ Sess11 (19th March) - React/gif-search/src/App.js
@@ -1,58 +1,60 @@
-import logo from "./images/giphy-logo.svg";
+import React from "react";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm/SearchForm";
+import ImageContainer from "./components/ImageContainer/ImageContainer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App d-flex justify-content-center">
-      <div id="main-container" className="d-flex flex-column align-items-center">
-        <div className="header">
-          <img src={logo} alt="" />
-          <h1>Search a Gif</h1>
-        </div>
-        <div className="form">
-          <input type="text" className="form-control" />
-          <button id="search-btn" className="btn btn-primary d-none">
-            Search
-          </button>
-        </div>
-        <div className="content">
-          <div className="image-container d-flex">
-            <div className="image-inner">
-              <img
-                src="https://media.giphy.com/media/LmYoDjbWL0RkFcRoeb/giphy.gif"
-                alt=""
-              />
-            </div>
-            <div className="image-caption">
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-            </div>
-          </div>
-          <div className="image-container d-flex">
-            <div className="image-inner">
-              <img
-                src="https://media.giphy.com/media/H0EBDM4Vk6880/giphy.gif"
-                alt=""
-              />
-            </div>
-            <div className="image-caption">
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-            </div>
-          </div>
-          <div className="image-container d-flex">
-            <div className="image-inner">
-              <img
-                src="https://media.giphy.com/media/Rxt5Nxbl3KM3DhyxZm/giphy.gif"
-                alt=""
-              />
-            </div>
-            <div className="image-caption">
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-            </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: "Gif me!!!!!",
+      images: [
+        {
+          imgUrl: "https://media.giphy.com/media/H0EBDM4Vk6880/giphy.gif",
+          caption: "shaq",
+        },
+        {
+          imgUrl: "https://media.giphy.com/media/Rxt5Nxbl3KM3DhyxZm/giphy.gif",
+          caption: "wiggle",
+        },
+        {
+          imgUrl: "https://media.giphy.com/media/LmYoDjbWL0RkFcRoeb/giphy.gif",
+          caption: "cat",
+        },
+      ],
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ label: "Let's Search!!!!" });
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <div className="App d-flex justify-content-center">
+        <div
+          id="main-container"
+          className="d-flex flex-column align-items-center"
+        >
+          <Header label={this.state.label}></Header>
+          <SearchForm></SearchForm>
+          <div className="content">
+            {this.state.images.map((image, idx) => {
+              return (
+                <ImageContainer
+                  key={idx}
+                  imgUrl={image.imgUrl}
+                  caption={image.caption}
+                ></ImageContainer>
+              );
+            })}
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ Sess11 (19th March) - React/gif-search/src/components/SearchForm/SearchForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { keyword: "" };
+  }
+
+  handleFormChange = (event) => {
+    console.log(event.target.value);
+    this.setState({ keyword: event.target.value });
+  };
+
+  renderButton() {
+    if (this.state.keyword.length > 0) {
+      return (
+        <button id="search-btn" className="btn btn-primary ms-2">
+          Search
+        </button>
+      );
+    }
+    return <button disabled>Search</button>;
+  }
+
+  render() {
+    return (
+      <div className="form d-flex">
+        <input
+          type="text"
+          className="form-control"
+          value={this.state.keyword}
+          onChange={this.handleFormChange}
+        />
+        
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
